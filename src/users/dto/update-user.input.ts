de1589e3f3e322616067358fe54c56707dc9af6b
Daddyjohn63 <john@webandprosper.co.uk>
2025-10-13 15:43:37 +0100

    JWT and Gql guards implemented

--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,9 +1,6 @@
 //UpdateUserInput: Input type for updating existing users
 import { CreateUserInput } from './create-user.input';
-import { InputType, Field, PartialType } from '@nestjs/graphql';
+import { InputType, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field()
-  _id: string;
-}
+export class UpdateUserInput extends PartialType(CreateUserInput) {}
